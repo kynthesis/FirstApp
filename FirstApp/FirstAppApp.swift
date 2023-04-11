c3696c63a81432680066e60bb451ecfe87a728a6
Khoa D. Vo <khoa.synthesis@gmail.com>
2023-04-11 15:46:09 +0700

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Khoa D. Vo on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
